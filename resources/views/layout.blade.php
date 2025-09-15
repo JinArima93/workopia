f0106f074b35ea0d4afb740284f8d97573b1ccc5
xKayden <85267466+JinArima93@users.noreply.github.com>
2025-09-15 19:59:48 +0800

    Section 5 Final

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <title>Workopia | Find and list jobs</title> -->
      <title>{{ $title ?? 'Workopia | Find and list jobs' }}</title>
+     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
      <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
@@ -12,14 +14,21 @@
         crossorigin="anonymous"
         referrerpolicy="no-referrer"
         />
+        
      @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-100">
     <x-header />
-    
+     @if(request()->is('/'))
+  @if(request()->is('/'))
+    <x-hero />
+    <x-top-banner />
+  @endif
+  @endif
     <main class="container mx-auto mt-4 p-4">
         {{ $slot }}
     </main>
 </body>
+<script src="{{ asset('js/script.js') }}"></script>
 </html>
 
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,5 +1,3 @@
 <x-layout>
-    <x-slot name="title">Welcome to Workopia</x-slot>
-    <h1 class="text-3xl font-bold mb-4">Welcome to Workopia</h1>
-    <p class="mb-4">Your gateway to finding and listing jobs.</p>
+    <x-bottom-banner />
 </x-layout>
